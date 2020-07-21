2965a5926b1afeb9e90188376bd4aaaac220be5e
vu1230204 <tamvu.nguyen.vtp@gmail.com>
2020-07-21 14:26:50 +0700

    Completed chapter7

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ApplicationRecord
   before_save :downcase_email
 
   validates :name, presence: true
-  VALIDATES_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALIDATES_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :email, presence: true, format: {with: VALIDATES_EMAIL_REGEX}
-    def downcase_email
-      email.downcase!
-    end
+  def downcase_email
+    email.downcase!
+  end
   has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get '/help', to:'static_pages#help'
+  get '/help',  to: 'static_pages#help'
+  get '/signup' ,to:'users#new'
+  resources :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
