55d86e77add84252e52aadbd6b9390b971d6da36
vu1230204 <tamvu.nguyen.vtp@gmail.com>
2020-07-21 14:26:50 +0700

    Completed chapter9

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,38 @@
 class User < ApplicationRecord
-  before_save :downcase_email
+  attr_accessor :remember_token
+  before_save{self.email = email.downcase}
 
   validates :name, presence: true
-  VALIDATES_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALIDATES_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :email, presence: true, format: {with: VALIDATES_EMAIL_REGEX}
-    def downcase_email
-      email.downcase!
-    end
+  # def downcase_email
+  #   email.downcase!
+  # end
   has_secure_password
+  # Returns the hash digest of the given string.
+  def self.digest string
+    cost = BCrypt::Engine::MIN_COST if ActiveModel::SecurePassword.min_cost
+    cost = BCrypt::Engine.cost unless ActiveModel::SecurePassword.min_cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    remember_token = User.new_token
+    update remember_digest: User.digest(remember_token)
+  end
+
+  def forget
+    update remember_digest: nil
+  end
+
+  # Returns true if the given token matches the digest.
+  def authenticated? remember_token
+    return false unless remember_digest
+
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'static_pages#home'
-  get '/help', to:'static_pages#help'
+  get '/help',  to: 'static_pages#help'
+  #signup,login
+  get '/signup',  to:'users#new'
+  get '/login',  to:'sessions#new'
+  post '/login',  to:'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  resources :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
